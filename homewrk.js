c1ab2d26d321d127904e871357c6505e253a209f
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-20 05:50:20 +0500

    jalol

---
+++ homewrk.js
@@ -0,0 +1,87 @@
+//Func1
+
+// const power = (a, n) => a ** n;
+
+// console.log(daraja(2, 5));
+
+//Func2
+
+// function mean(a, b) {
+//     midArif = (a + b) / 2;
+//     midSQRt = (a + b) ** (1 / 2) 
+//     console.log("O'rta Arifmetik", midArif);
+//     console.log("Ildiz ", midSQRt);
+// }
+
+// mean (12, 48)
+
+
+//Func3
+
+// const sign = (n) => {
+//     check = Math.sign(n);
+//     return  check;
+// }
+
+
+// console.log(sign(0));
+
+//Func4
+
+// const areaCircle = (r) => {
+//     let p = 3.14;
+//     S = p * r ** 2;
+
+//     return Math.round(S);
+// }
+ 
+// console.log(areaCircle(44));
+
+//Func5
+
+// const sumRange = (a,b) => {
+//     let sum = 0;
+//     if (a > b) {
+//         console.log(0);
+//     }else {
+//         for ( a ; a < b; a++) {
+//         sum += a;
+//     }
+//     }
+//      console.log(sum);
+// }
+
+// sumRange(9,12);
+
+
+//Func6
+
+// const calc = (a ,b ,c)  => {
+//     if (c == "*") {
+//         a * b
+//     }else if (c == "-"){
+//         a - b 
+//     }else if (c = "/") {
+//         a / b
+//     }else if (c = "+") {
+//         a + b
+//     }
+//     return console.log(c);
+// }
+
+// calc(1,2,"/");
+
+
+//Func7
+
+// const isEven = (k) => {
+//      if (k % 2 == 0) {
+//          console.log(true);
+//     }else {
+//         console.log(false);
+//     }
+// }
+
+// isEven(5);
+
+//Func7
